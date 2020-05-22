4dfd42305ea14cb9a11c7d75a1fd2daf447033e6
rohit kumar <rohit1071998@gmail.com>
2020-05-22 12:27:31 +0530

    EJS Layouts: add layouts in views folder

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,5 +1,9 @@
 
 // module.exports.actionName = function(err, res){};
+
 module.exports.home = function(err, res) {
-    return res.end('<h1> Express is set-up for codeial </h1>');
+      return res.render('home', {
+          title: "Home"
+      });
+    // return res.end('<h1> Express is set-up for codeial </h1>');
 }
--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile = function(req, res) {
-    res.end('<h1> Users Profile </h1>');
+
+    return res.render('user_profile',{
+          title: 'User profile'
+    });
+    // res.end('<h1> Users Profile </h1>');
 }
