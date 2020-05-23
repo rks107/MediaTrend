15fab8c1f0b904609f2820e68a0d581ee3e5c285
rohit kumar <rohit1071998@gmail.com>
2020-05-23 12:25:55 +0530

    Form created :: SignIN and SignUp

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -5,3 +5,29 @@ module.exports.profile = function(req, res) {
     });
     // res.end('<h1> Users Profile </h1>');
 }
+
+// render Sign in page
+module.exports.signIn = function(req, res) {
+
+    return res.render('user_sign_in', {
+        title : "Codeial | Sign In"
+    })
+}
+
+// render sign up page
+module.exports.signUp = function(req, res ){
+
+    return res.render('user_sign_up', {
+        title : "Codeial | Sign Up"
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res) {
+    // TODO Later
+}
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res) {
+    // TODO later
+}
