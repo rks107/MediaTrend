75ba3baee0fb20ee2299b62b7f49bf0c34fee484
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-08-28 10:35:30 +0530

    Add mailer template for new comment on post

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,13 +1,14 @@
 const nodeMailer = require('../config/nodemailer');
 
 exports.newComment = (comment) => {
-   console.log('inside newComment mailer', comment);
+//    console.log('inside newComment mailer', comment);
+   let htmlString = nodeMailer.renderTemplate({comment:comment}, '/comments/new_comment.ejs');
 
    nodeMailer.transpoter.sendMail({
        from: 'developer.rks107@gmail.com',
        to: comment.user.email,
        subject: 'New Comment published',
-       html: '<h1> Yup, your comment is published now!</>'
+       html: htmlString
    }, (err, info) => {
         if(err){console.log('Error in sending mail', err); return;}
   
